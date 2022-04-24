cf71bb587efb14e156529a89d68c2e8f4d09d5a7
Karen Payne <paynekaren@comcast.net>
2022-04-24 04:14:12 -0700

    added new sample

--- OccurrencesApp/Classes/Operations.cs
+++ OccurrencesApp/Classes/Operations.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace OccurrencesApp.Classes
 {
     public class Operations
     {
-        /// <summary>
-        /// Count occurrences for each char in a string
-        /// </summary>
-        public static List<Item> GetAllItems(string values)
+
+        public static List<Item> GetAllItems(string values) 
+            => (from item in ProcessData(values) select item).ToList();
+
+        public static List<List<Item>> ReadFromFile()
+        {
+            var itemList = new List<List<Item>>();
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data.txt");
+            var lines = File.ReadAllLines(fileName);
+
+            foreach (var line in lines)
+            {
+                itemList.Add((from item in ProcessData(line) select item).ToList());
+            }
+
+            return itemList;
+        }
+
+        private static IOrderedEnumerable<Item> ProcessData(string values)
         {
             var itemsGroup = (
                     from chr in values.ToCharArray()
-                    group chr by chr into grp
+                    group chr by chr
+                    into grp
                     select new Item
                     {
                         Character = grp.Key,
@@ -23,28 +40,7 @@ namespace OccurrencesApp.Classes
                 .ToList()
                 .OrderBy(item => item.Character.ToString());
 
-            return (from item in itemsGroup select item).ToList();
-
-        }
-
-        /// <summary>
-        /// Count occurrences of numbers in array
-        /// </summary>
-        public static List<NumberItem> GetAllNumbers(int[] values)
-        {
-            IOrderedEnumerable<NumberItem> itemsGroup = (
-                    from chr in values
-                    group chr by chr into grp
-                    select new NumberItem
-                    {
-                        Item = grp.Key,
-                        Occurrences = grp.Count()
-                    })
-                .ToList()
-                .OrderBy(item => item.Item.ToString());
-
-            return (from item in itemsGroup select item).ToList();
-
+            return itemsGroup;
         }
     }
 }
--- OccurrencesApp/Form1.cs
+++ OccurrencesApp/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using OccurrencesApp.Classes;
 
@@ -30,6 +25,38 @@ namespace OccurrencesApp
                 }
                 
             }
+
+            items = items = Operations.GetAllItems(textBox2.Text);
+
+            foreach (var item in items)
+            {
+                if (char.IsDigit(item.Character))
+                {
+                    listView2.Items.Add(new ListViewItem(item.ItemArray));
+                }
+
+            }
+
+        }
+
+        private void ReadFileButton_Click(object sender, EventArgs e)
+        {
+            List<Item> GetInfo(List<List<Item>> list, int findChar)
+            {
+                var items = 
+                    (
+                        from sublist in list 
+                        from item in sublist 
+                        where item.Character == findChar select item).ToList();
+                return items;
+            }
+
+            List<List<Item>> itemList = Operations.ReadFromFile();
+            
+            var one = GetInfo(itemList, '1').Sum(x => x.Occurrences);
+            var eight = GetInfo(itemList, '8').Sum(x => x.Occurrences);
+            Console.WriteLine($"1 = {one,-4}8 = {eight}");
+   
         }
     }
 }
