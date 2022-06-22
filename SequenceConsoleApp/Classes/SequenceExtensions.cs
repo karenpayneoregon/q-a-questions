f2ca1aa13ea9f120699010cdf364dd083de9e960
Karen Payne <paynekaren@comcast.net>
2022-06-22 05:50:13 -0700

    added console lib and Sequence project

---
+++ SequenceConsoleApp/Classes/SequenceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace SequenceConsoleApp.Classes
+{
+    public static class SequenceExtensions
+    {
+        /// <summary>
+        /// Get missing elements
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <returns>missing elements</returns>
+        /// <remarks>Run <seealso cref="IsSequenceBroken"/> first to determine if elements are missing</remarks>
+        public static int[] SequenceFindMissing(this int[] sequence) =>
+            sequence.Zip(sequence.Skip(1), (valueLeft, valueRight)
+                => Enumerable.Range(valueLeft + 1, (valueRight - valueLeft) - 1))
+                .SelectMany(item => item)
+                .ToArray();
+
+        /// <summary>
+        /// Determine if the sequence has missing elements
+        /// </summary>
+        /// <param name="sequence">int array</param>
+        /// <returns>true if missing elements, false if no missing elements</returns>
+        public static bool IsSequenceBroken(this int[] sequence) =>
+            sequence.Zip(sequence.Skip(1), (valueLeft, valueRight)
+                => valueRight - valueLeft).Any(item => item != 1);
+    }
+}
---
+++ SequenceConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using SequenceConsoleApp.Classes;
+
+
+namespace SequenceConsoleApp
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 1, 2, 3, 5, 6, 8, 10 };
+            
+            if (array.IsSequenceBroken())
+            {
+                var test = array.SequenceFindMissing();
+                Console.WriteLine(string.Join(",", array.SequenceFindMissing()));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
