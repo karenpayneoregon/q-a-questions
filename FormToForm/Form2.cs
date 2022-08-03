0e5493127bc94174204f1d3c936057a9f246966f
Karen Payne <paynekaren@comcast.net>
2022-08-03 13:24:11 -0700

    new code

---
+++ FormToForm/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormToForm
+{
+    public partial class Form2 : Form
+    {
+        public delegate void OnPassData(bool value);
+        public event OnPassData PassEvent;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PassEvent?.Invoke(checkBox1.Checked);
+        }
+    }
+}
