a39051059b2c450b4df5c8c38d9b5720f45c2353
Karen Payne <paynekaren@comcast.net>
2022-08-05 10:59:32 -0700

    touch-up

--- GetImageFromSqlServerFormApp/Classes/DataOperations.cs
+++ GetImageFromSqlServerFormApp/Classes/DataOperations.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace GetImageFromSqlServerFormApp.Classes
 {
     public class DataOperations
     {
-        public static string ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=WorkingImages;Integrated Security=True";
+        public static string ConnectionString = 
+            "Data Source=.\\sqlexpress;Initial Catalog=WorkingImages;Integrated Security=True";
 
         /// <summary>
         /// Get record count
---
+++ GetImageFromSqlServerFormApp/readme.md
@@ -0,0 +1,4 @@
+﻿
+Simple example to read an image from SQL-Server to a PictureBox
+
+![o](figure2.png)
