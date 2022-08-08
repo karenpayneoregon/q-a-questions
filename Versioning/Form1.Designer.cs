d50dbce9c04f1c413b1b2eb3803a99253ef5af58
Karen Payne <paynekaren@comcast.net>
2022-08-08 06:46:46 -0700

    new code

--- Versioning/Form1.Designer.cs
+++ Versioning/Form1.Designer.cs
@@ -30,38 +30,28 @@ namespace Versioning
         private void InitializeComponent()
         {
             this.GetVersionButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // GetVersionButton
             // 
-            this.GetVersionButton.Location = new System.Drawing.Point(22, 31);
+            this.GetVersionButton.Location = new System.Drawing.Point(129, 49);
             this.GetVersionButton.Name = "GetVersionButton";
             this.GetVersionButton.Size = new System.Drawing.Size(75, 23);
             this.GetVersionButton.TabIndex = 0;
-            this.GetVersionButton.Text = "button1";
+            this.GetVersionButton.Text = "Execute";
             this.GetVersionButton.UseVisualStyleBackColor = true;
             this.GetVersionButton.Click += new System.EventHandler(this.GetZerosButton_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(77, 69);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(332, 120);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.GetVersionButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code sample";
             this.ResumeLayout(false);
 
         }
@@ -69,7 +59,6 @@ namespace Versioning
         #endregion
 
         private System.Windows.Forms.Button GetVersionButton;
-        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ WinApp1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WinApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnglishComboBox1 = new System.Windows.Forms.ComboBox();
+            this.TimeZonesCombox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // EnglishComboBox1
+            // 
+            this.EnglishComboBox1.FormattingEnabled = true;
+            this.EnglishComboBox1.Location = new System.Drawing.Point(12, 27);
+            this.EnglishComboBox1.Name = "EnglishComboBox1";
+            this.EnglishComboBox1.Size = new System.Drawing.Size(188, 21);
+            this.EnglishComboBox1.TabIndex = 0;
+            // 
+            // TimeZonesCombox
+            // 
+            this.TimeZonesCombox.FormattingEnabled = true;
+            this.TimeZonesCombox.Location = new System.Drawing.Point(219, 27);
+            this.TimeZonesCombox.Name = "TimeZonesCombox";
+            this.TimeZonesCombox.Size = new System.Drawing.Size(188, 21);
+            this.TimeZonesCombox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 79);
+            this.Controls.Add(this.TimeZonesCombox);
+            this.Controls.Add(this.EnglishComboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code Sample";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox EnglishComboBox1;
+        private System.Windows.Forms.ComboBox TimeZonesCombox;
+    }
+}
+
