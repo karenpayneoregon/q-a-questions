9c5879105b54d3eaabbce928b6e4021619f2c5ba
Karen Payne <paynekaren@comcast.net>
2022-08-31 04:05:40 -0700

    namespace file

--- WinFormsAppKarenVersion/Controls/CustomTextBox.cs
+++ WinFormsAppKarenVersion/Controls/CustomTextBox.cs
@@ -2,55 +2,53 @@
 using System.Globalization;
 using System.Runtime.CompilerServices;
 
-namespace WinFormsAppKarenVersion.Controls
+namespace WinFormsAppKarenVersion.Controls;
+
+public class CustomTextBox : TextBox, INotifyPropertyChanged
 {
+    public delegate void ValueChanged(double value);
+    public event ValueChanged OnValueChanged = null!;
 
-    public class CustomTextBox : TextBox, INotifyPropertyChanged
+    private double _doubleValue;
+
+    public double Value
     {
-        public delegate void ValueChanged(double value);
-        public event ValueChanged OnValueChanged = null!;
-
-        private double _doubleValue;
-
-        public double Value
+        get => _doubleValue;
+        set
         {
-            get => _doubleValue;
-            set
+            _doubleValue = value;
+            OnPropertyChanged();
+        }
+    }
+
+    [Category("Custom")]
+    [Description("Override value for WatchValue")]
+    public double OverRideValue { get; set; }
+
+    [Category("Custom")]
+    [Description("Value to watch and change")]
+    public double WatchValue { get; set; }
+
+    protected override void OnTextChanged(EventArgs e)
+    {
+        if (double.TryParse(Text, out var value))
+        {
+            if (value == WatchValue)
             {
-                _doubleValue = value;
-                OnPropertyChanged();
+                Text = OverRideValue.ToString(CultureInfo.InvariantCulture);
+                OnValueChanged?.Invoke(30);
+            }
+            else
+            {
+                OnValueChanged?.Invoke(value);
             }
         }
-
-        [Category("Custom")]
-        [Description("Override value for WatchValue")]
-        public double OverRideValue { get; set; }
-
-        [Category("Custom")]
-        [Description("Value to watch and change")]
-        public double WatchValue { get; set; }
-
-        protected override void OnTextChanged(EventArgs e)
-        {
-            if (double.TryParse(Text, out var value))
-            {
-                if (value == WatchValue)
-                {
-                    Text = OverRideValue.ToString(CultureInfo.InvariantCulture);
-                    OnValueChanged?.Invoke(30);
-                }
-                else
-                {
-                    OnValueChanged?.Invoke(value);
-                }
-            }
-            base.OnTextChanged(e);
-        }
+        base.OnTextChanged(e);
+    }
         
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null!)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null!)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
