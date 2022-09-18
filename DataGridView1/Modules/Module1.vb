2a9f235cbfc8ecb1bf91d96cfba9c22d58bf919d
Karen Payne <paynekaren@comcast.net>
2022-09-17 18:48:57 -0700

    renamed vars

--- DataGridView1/Modules/Module1.vb
+++ DataGridView1/Modules/Module1.vb
@@ -1,15 +1,15 @@
 ﻿Module Module1
     <DebuggerStepThrough()>
     <Runtime.CompilerServices.Extension()>
-    Public Function GetCheckedRows1(ByVal GridView As DataGridView, ByVal ColumnName As String) As List(Of DataGridViewRow)
+    Public Function GetCheckedRows1(ByVal gridView As DataGridView, ByVal columnName As String) As List(Of DataGridViewRow)
         Return _
             (
-                From SubRows In
+                From subRows In
                     (
-                        From Rows In GridView.Rows.Cast(Of DataGridViewRow)()
-                            Where Not Rows.IsNewRow
+                        From Rows In gridView.Rows.Cast(Of DataGridViewRow)()
+                        Where Not Rows.IsNewRow
                         ).ToList
-                    Where CBool(SubRows.Cells(ColumnName).Value) = True
+                Where CBool(subRows.Cells(columnName).Value) = True
                 ).ToList
     End Function
 End Module
