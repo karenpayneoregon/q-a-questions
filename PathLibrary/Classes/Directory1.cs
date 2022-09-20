92919f99721e35c83ccef9cef05fbcc1cad05042
Karen Payne <paynekaren@comcast.net>
2022-09-20 03:29:20 -0700

    name change and docs

--- PathLibrary/Classes/Directory1.cs
+++ PathLibrary/Classes/Directory1.cs
@@ -4,10 +4,15 @@ namespace PathLibrary.Classes;
 
 public class Directory1
 {
-    private static readonly Regex _illegalRegex = 
+    private static readonly Regex IllegalRegex = 
         new($"[{Regex.Escape(new string(Path.GetInvalidFileNameChars()))}]", 
             RegexOptions.Compiled);
 
+    /// <summary>
+    /// Determine if <see cref="folder"/> is valid
+    /// </summary>
+    /// <param name="folder">directory to check</param>
+    /// <returns>true if valid, false if not</returns>
     public static bool IsValidFolderName(string folder) 
-        => _illegalRegex.Replace(folder,"") == folder;
+        => IllegalRegex.Replace(folder,"") == folder;
 }
