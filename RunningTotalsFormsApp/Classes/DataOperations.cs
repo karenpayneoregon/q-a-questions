5d42b755ba21069e21b1052aeb7b9b75fc5f1cda
Karen Payne <paynekaren@comcast.net>
2023-08-03 06:14:03 -0700

    cleanup

--- RunningTotalsFormsApp/Classes/DataOperations.cs
+++ RunningTotalsFormsApp/Classes/DataOperations.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RunningTotalsFormsApp.Classes;
 internal class DataOperations
