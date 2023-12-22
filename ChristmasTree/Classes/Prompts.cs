48493c226bc3c24b322bc4b8b75068b6d39c9739
Karen Payne <paynekaren@comcast.net>
2023-12-22 13:40:51 -0800

    :curly_loop:

--- ChristmasTree/Classes/Prompts.cs
+++ ChristmasTree/Classes/Prompts.cs
@@ -6,5 +6,5 @@ internal class Prompts
             new TextPrompt<T>($"[white]{text}[/]?")
                 .PromptStyle("yellow")
                 .DefaultValue(value)
-                .ValidationErrorMessage("[red]Please enter a numbers[/]"));
+                .ValidationErrorMessage("[red]Please enter a number[/]"));
 }
